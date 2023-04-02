fabac67d2526856c062335e90ee6f34acff20642
alghifarydaffa62 <88295192+alghifarydaffa62@users.noreply.github.com>
2023-04-02 11:36:00 +0700

    start

---
+++ robotika/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <style>{'body { background-color: #000080; }'}</style>
+    </div>
+  );
+}
+
+export default App;
