8f6a19a285f2ddac62e0b5ee84035ab7d6f8a8fd
alghifarydaffa62 <88295192+alghifarydaffa62@users.noreply.github.com>
2023-04-02 11:50:18 +0700

    edit bg

--- robotika/src/App.js
+++ robotika/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" >
-      <style>{'body { background-color: #000080; }'}</style>
+      <style>{'body { background-color: #192841; }'}</style>
     </div>
   );
 }
