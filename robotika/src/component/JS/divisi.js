feb5d66fae1abd6b6848b1a5ec694a8863b4503a
alghifarydaffa62 <88295192+alghifarydaffa62@users.noreply.github.com>
2023-04-06 15:37:08 +0700

    update divisi

--- robotika/src/component/JS/divisi.js
+++ robotika/src/component/JS/divisi.js
@@ -4,12 +4,12 @@ import cardrobotic from '../img/cardrobotic.jpg'
 import cardwebdev from '../img/webdevcard.jpg'
 import webdevicon from '../img/programming.png'
 import programmingicon from '../img/coding-language.png'
-import roboticicon from '../img/ai/png'
+import roboticicon from '../img/ai.png'
 
 const Divisi = () => {
-    return(
+    return (
         <div class="divisi">
-            <h3>Divisi</h3>
+            <h2>Divisi</h2>
 
             <div class="rowdiv">
                 <div class="div1">
@@ -23,6 +23,29 @@ const Divisi = () => {
                 <div class="div3">
                     <img src={roboticicon}></img>
                     <p>Robotic</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="web">
+                    <img src={cardwebdev}></img>
+                    <h4>Web Developer</h4>
+                    <p>Sebuah divisi yang megkhususkan<br></br>dalam programming 
+                    dan<br></br>mengembangkan website</p>
+                </div>
+
+                <div class="programming">
+                    <img src={cardprogramming}></img>
+                    <h4>Programming</h4>
+                    <p>Sebuah divisi yang mengkhususkan<br></br>dalam programming secara general 
+                    <br></br>dan luas</p>
+                </div>
+
+                <div class="robotic">
+                    <img src={cardrobotic}></img>
+                    <h4>Robotic</h4>
+                    <p>Sebuah divisi yang mengkhususkan<br></br>dalam mengembangkan 
+                    dan<br></br>membuat robot</p>
                 </div>
             </div>
         </div>
